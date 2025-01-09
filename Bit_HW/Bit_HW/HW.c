cfcdc7ecdf93e8d4ec3f8f3febbb852dc524a098
Cmx <Spiralgalaxy018@gmail.com>
2025-01-09 10:45:10 +0800

    Modified

--- Bit_HW/Bit_HW/HW.c
+++ Bit_HW/Bit_HW/HW.c
@@ -22,6 +22,43 @@ void Bubble_Sort(int *arr, size_t sz) {
 	}
 }
 
+int findRound(const char* src, char* find)
+{
+	char tmp[256] = { 0 }; //用一个辅助空间将原字符串做成两倍原字符串
+	strcpy(tmp, src); //先拷贝一遍
+	strcat(tmp, src); //再连接一遍
+	return (strstr(tmp, find) != NULL); //看看找不找得到
+}
+
+
+void menu() {
+	printf("********************************************\n");
+	printf("*           選擇您要執行的功能:            *\n");
+	printf("*           1. add       2. sub            *\n");
+	printf("*           3. mul       4. div            *\n");
+	printf("********************************************\n");
+}
+
+int add(int a, int b) {
+	return a + b;
+}
+
+int sub(int a, int b) {
+	return a - b;
+}
+
+int mul(int a, int b) {
+	return a * b;
+}
+
+int div(int a, int b) {
+	return a / b;
+}
+
+
+
+
+
 int main() {
 	#pragma region 猜兇手
 	/*
@@ -89,31 +126,121 @@ int main() {
 
 	// 一維數組的方式實現
 
-	int curr[20] = { 0 }, prev[20] = { 0 };
-	int n;
-	scanf("%d", &n);
+	//int curr[20] = { 0 }, prev[20] = { 0 };
+	//int n;
+	//scanf("%d", &n);
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j <= i; j++) {
-			if (j == 0 || j == i) {
-				curr[j] = 1;
-			}
-			else {
-				// 現在的第j個就是上一輪的 第j個 + 第j-1個
-				curr[j] = prev[j - 1] + prev[j];
-				
-			}
-			printf("%d ", curr[j]);
-		}
-		printf("\n");
-		// 更新數據
-		for (int k = 0; k <= i; k++) {
-			prev[k] = curr[k];
-		}
-	}
+	//for (int i = 0; i < n; i++) {
+	//	for (int j = 0; j <= i; j++) {
+	//		if (j == 0 || j == i) {
+	//			curr[j] = 1;
+	//		}
+	//		else {
+	//			// 現在的第j個就是上一輪的 第j個 + 第j-1個
+	//			curr[j] = prev[j - 1] + prev[j];
+	//			
+	//		}
+	//		printf("%d ", curr[j]);
+	//	}
+	//	printf("\n");
+	//	// 更新數據
+	//	for (int k = 0; k <= i; k++) {
+	//		prev[k] = curr[k];
+	//	}
+	//}
 	#pragma endregion
 
-	
+	#pragma region 杨氏矩阵
+	//int arr[4][4] = { {1,4,7,8}, {2,5,8,9},{3,6,10,11},{10,15,20,25} };
+
+	//int target;
+	//int rows =  0 , cols = 3;
+	//scanf("%d", &target);
+
+	//// 从矩阵的最右上的元素开始遍历
+	//while ( (rows < 4 ) && (cols >= 0)) {
+	//	if (arr[rows][cols] == target) {
+	//		printf("%d found in matrix\n", target);
+	//		break;
+	//	}
+	//	else if (arr[rows][cols] > target) { // 若当前值大于target
+	//		cols--;
+	//	}
+	//	else { // 当前值小于target
+	//		rows++;
+	//	}
+
+
+	//}
+
+#pragma endregion
+
+	#pragma region 字符串旋转结果
+	/*
+	例如：给定s1 =AABCD和s2 = BCDAA，返回1
+
+	给定s1=abcd和s2=ACBD，返回0.
+
+	AABCD左旋一个字符得到ABCDA
+	AABCD左旋两个字符得到BCDAA
+	AABCD右旋一个字符得到DAABC
+	*/
+
+	//const char* str1 = "abcde";
+	//const char* str2 = "bcdea";
+	//printf("%d", findRound(str1, str2));
+#pragma endregion
+
+	#pragma region 轉移表
+
+
+	//int input = 0;
+	//menu();
+	//
+
+	//int (*p[5])(int x, int y) = {0,add,sub,mul,div};
+
+	//int num1, num2;
+
+	//do {
+	//	printf("輸入您要運行的功能:");
+	//	scanf("%d", &input);
+	//	if (input <= 4 && input > 0) {
+	//		printf("輸入操作數:");
+	//		scanf("%d %d", &num1, &num2);
+	//		int ret = (*p[input])(num1, num2);
+	//		printf("Result:%d\n", ret);
+	//	}
+	//	else if (input == 0) {
+	//		printf("Exit\n");
+	//	}
+	//	else {
+	//		printf("輸入錯誤，重新輸入\n");
+	//	}
+	//} while (input);
+	#pragma endregion
+
+#pragma region 單身狗2
+
+int num[] = { 1,2,3,4,5,1,2,3,4,6 };
+size_t sz = sizeof(num) / sizeof(num[0]);
+
+for (int i = 0; i < sz; i++) {
+	int count = 1;
+	for (int j = i+1; j < sz; j++) {
+		if (num[i] == num[j]) {
+			count++;
+			num[j] = 0;
+		}
+
+	}
+	if (count == 1 && num[i] != 0) {
+		printf("%d ", num[i]);
+	}
+
+}
+
+#pragma endregion
 
 	return 0;
 }
--- String/String/String.c
+++ String/String/String.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 #pragma region strlen()實現
 
@@ -42,13 +43,116 @@
 
 #pragma endregion
 
+#pragma region strcpy()實現
 
+char* my_strcpy (char* dest, const char* sour) {
+
+	char* ret = dest;
+	while (*dest++ = *sour) {
+		;
+	}
+	return ret;
+}
+
+#pragma endregion
+
+
+#pragma region strcat()實現
+char* my_strcat(char* dest, const char* sour) {
+	assert(dest && sour);
+	
+	char* ret = dest;
+	
+	while (*dest != '\0') {
+		dest++;
+	}
+
+	while (*dest++ = *sour++) {
+		;
+	}
+	return ret;
+}
+#pragma endregion
+
+#pragma region strcmp()實現
+
+// 字符串比較是比較對應位置的大小
+
+int my_strcmp(char* str1, char* str2) {
+	
+	while (*str1 == *str2) {
+		if (*str1 == '\0') {
+
+			return 0;
+		}
+		str1++;
+		str2++;
+	}
+	return (*str1 - *str2);
+
+}
+
+#pragma endregion
+
+#pragma region strstr()實現
+
+char* my_strstr(const char* str1, const char* str2) {
+
+	const char* s1 = NULL;
+	const char* s2 = NULL;
+	const char* cur = str1;
+
+	while (*cur ) { // *cur != '\0'
+		s1 = cur;
+		s2 = str2;
+
+		//特殊情況 -- str2指向的是空字符串，直接返回str1
+
+		if (*str2 == '\0') {
+			return str1;
+		}
+
+		while (*s1  && *s2  &&  * s1 == *s2) {
+			s1++;
+			s2++;
+		}
+		if (*s2 == '\0') {
+			return cur;
+		}
+		cur++;
+	}
+}
+
+#pragma endregion
 
 
 int main() {
 
-	
+#pragma region strtok
 
+	//char arr[] = "cmx@yeah.net";
+	//char buf[256] = { 0 };
+	//strcpy(buf, arr);
+	//char* sep = "@.";
+	//char* ret = NULL;
+
+	//for (ret = strtok(buf, sep); ret != NULL; ret = strtok(NULL, sep)) {
+	//	printf("%s\n", ret);
+	//}
+
+
+#pragma endregion
+
+	char arr[] = "abcdefabcdef";
+	char* p = "cdef";
+	const char* ret = my_strstr(arr, p);
+
+	if (ret == NULL) {
+		printf("不存在\n");
+	}
+	else {
+		printf("%s\n", ret);
+	}
 
 	return 0;
 }
